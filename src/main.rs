eb4eeab61a989b2c2db302ab065db4d3d079d54e
Simón Oroño <simonorono@protonmail.com>
2023-12-03 21:27:45 -0400

    Start implementing a command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    DlInput {
+        #[arg(long_help = "Your session cookie")]
+        session_cookie: String,
+
+        #[arg(short, long, long_help = "Until which day to download inputs")]
+        until_day: Option<i32>,
+    },
+    Run,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{:?}", cli)
 }
